30565463e550dd5b47868a881aca082d4c16eeb5
unknown <Tony@.(none)>
2011-03-25 16:43:50 -0700

    TransportObject 70% done

---
+++ base/src/edu/berkeley/cs/cs162/ServerReply.java
@@ -0,0 +1,15 @@
+package edu.berkeley.cs.cs162;
+
+public enum ServerReply {
+	OK,
+	REJECTED,
+	QUEUED,
+	OK_CREATE,
+	OK_JOIN,
+	BAD_GROUP,
+	FAIL_FULL,
+	NOT_MEMBER,
+	BAD_DEST,
+	FAIL,
+	FAILED
+}

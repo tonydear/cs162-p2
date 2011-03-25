39cda7cf654dd36ab31ae5d7cc4357654a247655
Paul <paul.ruan@gmail.com>
2011-03-25 15:18:18 -0700

    chatclient constructor done

--- base/src/edu/berkeley/cs/cs162/ChatClient.java
+++ base/src/edu/berkeley/cs/cs162/ChatClient.java
@@ -1,8 +1,11 @@
 package edu.berkeley.cs.cs162;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.HashMap;
 import java.util.Map;
 
 
@@ -11,12 +14,26 @@ public class ChatClient extends Thread{
 	private Map<String,ChatLog> logs;
 	private InputStream commands;
 	private ObjectInputStream received;
+	private ObjectOutputStream sent;
 	private Thread receiver;
 	private boolean connected;
 	private Message reply; //what should reply from server look like
 	private volatile boolean isWaiting; //waiting for reply from server?
 	
 	public ChatClient(){
+		mySocket = new Socket();
+		logs = new HashMap<String,ChatLog>();
+		try {
+			received = new ObjectInputStream(mySocket.getInputStream());
+			sent = new ObjectOutputStream(mySocket.getOutputStream());
+			commands = System.in;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		connected = false;
+		isWaiting = false;
+		reply = null;
 		receiver = new Thread(){
             @Override
             public void run(){
@@ -27,7 +44,6 @@ public class ChatClient extends Thread{
         };
         receiver.start();
         start();
-        //changed
 	}
 	
 	private boolean connect(String hostname, int port){

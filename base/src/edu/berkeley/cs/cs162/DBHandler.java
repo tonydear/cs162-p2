61120a16c7b519d081ca14a7b677c92fc8371041
harryj <flamingfurball89@yahoo.com>
2011-04-13 19:56:17 -0700

    added sql stuff to dbhandler

--- base/src/edu/berkeley/cs/cs162/DBHandler.java
+++ base/src/edu/berkeley/cs/cs162/DBHandler.java
@@ -1,7 +1,9 @@
 package edu.berkeley.cs.cs162;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
 
 
@@ -21,5 +23,33 @@ public class DBHandler {
             e.printStackTrace();
         }
         
+    }
+    
+    public static void addUser(String username, String salt, String hashedPassword) throws SQLException {
+    	Statement stmt = conn.createStatement();
+    	stmt.executeQuery("INSERT into  users (username, salt, encrypted_password) VALUES + (" + username + "," + salt +"," + hashedPassword + ")");
+    	
+    }
+    
+    public static String getSalt(String username) throws SQLException {
+    	Statement stmt = conn.createStatement();
+    	ResultSet rs = stmt.executeQuery("SELECT * FROM users where username = " + username);
+    	rs.next();
+    	String salt = rs.getString("salt");
+    	return salt;
+    	
+    }
+    
+    public static void removeFromGroup(String uname, String gname) {
+    	
+    }
+    
+    public static String getHashedPassword(String uname) throws SQLException {
+    	Statement stmt = conn.createStatement();
+    	ResultSet rs = stmt.executeQuery("SELECT * FROM users where username = " + uname);
+    	rs.next();
+    	String hashedPassword = rs.getString("encrypted_password");
+    	return hashedPassword;
+    	
     }
 }

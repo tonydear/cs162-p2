58b2a0eeb2e81182b8a5e8cd846bcee8115d6a7f
Paul <paul.ruan@gmail.com>
2011-04-18 18:21:55 -0700

    receiver and sender benchmarker

---
+++ base/src/edu/berkeley/cs/cs162/BenchmarkSender.java
@@ -0,0 +1,11 @@
+package edu.berkeley.cs.cs162;
+
+import java.util.Map;
+
+public class BenchmarkSender extends ChatClient {
+	private Map<Integer,Long> benchmarkTimes;
+	
+	public BenchmarkSender(){
+		
+	}
+}

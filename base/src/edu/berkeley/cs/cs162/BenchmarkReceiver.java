5421542cbb23d5258087171af28085807a8c3047
Paul <paul.ruan@gmail.com>
2011-04-18 21:40:02 -0700

    abstract class for client

--- base/src/edu/berkeley/cs/cs162/BenchmarkReceiver.java
+++ base/src/edu/berkeley/cs/cs162/BenchmarkReceiver.java
@@ -3,9 +3,9 @@ package edu.berkeley.cs.cs162;
 import java.io.EOFException;
 import java.net.SocketException;
 
-public class BenchmarkReceiver extends ChatClient {
+public class BenchmarkReceiver extends AbstractChatClient {
 	@Override
-	protected void benchmark(TransportObject recObject) {
+	protected synchronized void benchmark(TransportObject recObject) {
 		send(recObject.getSender(),recObject.getSQN(),recObject.getMessage() + "ghost");
 	}
 }
--- base/src/edu/berkeley/cs/cs162/BenchmarkSender.java
+++ base/src/edu/berkeley/cs/cs162/BenchmarkSender.java
@@ -1,19 +1,20 @@
 package edu.berkeley.cs.cs162;
 
 import java.io.IOException;
+import java.net.UnknownHostException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
-public class BenchmarkSender extends ChatClient {
+public class BenchmarkSender extends AbstractChatClient {
 	private Map<Integer,Long> benchmarkTimes = new HashMap<Integer,Long>();
 	private Lock sendLock = new ReentrantLock();
 	
 	@Override
 	protected void send(String dest, int sqn, String msg){
 		sendLock.lock();
-		send(dest,sqn,msg);
+		super.send(dest,sqn,msg);
 		benchmarkTimes.put(sqn,System.currentTimeMillis());
 		sendLock.unlock();
 	}
@@ -27,6 +28,7 @@ public class BenchmarkSender extends ChatClient {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		System.out.println("Sending rtt");
 		sendLock.unlock();
 	}
 	
@@ -37,6 +39,10 @@ public class BenchmarkSender extends ChatClient {
 		long receivedTime = System.currentTimeMillis();
 		double rrt = (receivedTime - sentTime)/1000;
 		sendRTT(rrt);
+	}
+	
+	public static void main(String[] args) throws UnknownHostException{
+		new BenchmarkSender();
 	}
 	
 }

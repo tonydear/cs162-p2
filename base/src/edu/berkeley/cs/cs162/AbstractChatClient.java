af6894cd73623df0ff8e95cfe2685599545f76ae
Jason Jee <jj41190@gmail.com>
2011-04-18 22:37:25 -0700

    removed unused import

--- base/src/edu/berkeley/cs/cs162/AbstractChatClient.java
+++ base/src/edu/berkeley/cs/cs162/AbstractChatClient.java
@@ -9,7 +9,6 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.net.SocketException;
-import java.net.UnknownHostException;
 import java.util.HashMap;
 import java.util.Map;
 

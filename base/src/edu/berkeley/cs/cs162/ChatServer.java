8b36c6cb4da44011e8d7f43138c66da4c6b40a82
Jason Jee <jj41190@gmail.com>
2011-04-19 19:44:42 -0700

    thread testing

--- base/src/edu/berkeley/cs/cs162/ChatServer.java
+++ base/src/edu/berkeley/cs/cs162/ChatServer.java
@@ -661,6 +661,7 @@ public class ChatServer extends Thread implements ChatServerInterface {
 				chatServer.shutdown();
 			} else if (tokens[0].equals("thread-count")) {
 				System.err.println(Thread.activeCount());
+				Thread.currentThread().getThreadGroup().list();
 			}
 		}
 	}
